9d3f3f558ae58d9a74d7f7394f6dd47c8ae7e9d7
Dongxuan Li <31972255+dongxli@users.noreply.github.com>
2020-12-20 18:55:32 -0800

    portfolio

---
+++ src/portfolio.js
@@ -0,0 +1,355 @@
+﻿/* Change this file to get your personal Portfolio */
+
+// Your Summary And Greeting Section
+
+import emoji from "react-easy-emoji";
+
+const greeting = {
+  /* Your Summary And Greeting Section */
+  username: "Dongxuan Li",
+  title: "Hi, I'm David!",
+  subTitle: emoji(
+    "A passionate Software Engineer having Web development experiences in .Net, PostgreSQL, and Angular"
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1XIxnMvdtGdCgNi8s1PZhWWIBvvQNo7Pl/view?usp=sharing",
+};
+
+// Your Social Media Link
+
+const socialMediaLinks = {
+  github: "https://github.com/dongxli",
+  linkedin: "https://www.linkedin.com/in/david-li-185655148/",
+  gmail: "dongxli710@gmail.com",
+};
+
+// Your Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+    ),
+    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji(
+      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+    ),
+  ],
+
+  /* Make Sure You include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt",
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js",
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react",
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node",
+    },
+    {
+      skillName: "swift",
+      fontAwesomeClassname: "fab fa-swift",
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm",
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database",
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "fab fa-aws",
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire",
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python",
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "fab fa-docker",
+    },
+  ],
+};
+
+// Your education background
+
+const educationInfo = {
+  viewEducation: true, // Set it to true to see education section
+  schools: [
+    {
+      schoolName: "Georgia Institute of Technology",
+      logo: require("./assets/images/gatechLogo.png"),
+      subHeader: "Master of Science in Computer Science",
+      duration: "August 2020 - Present",
+      // desc: "Participated in the research of XXX and published 3 papers.",
+      // descBullets: [
+      //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      // ],
+    },
+    {
+      schoolName: "California State University, Long Beach",
+      logo: require("./assets/images/csulbLogo.png"),
+      subHeader: "Bachelor of Science in Computer Science",
+      duration: "August 2015 - May 2020",
+      // desc:
+      //   "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+      // descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"],
+    },
+  ],
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: false, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "90%", //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "70%",
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "60%",
+    },
+  ],
+};
+
+// Your top 3 work experiences
+
+const workExperiences = {
+  viewExperiences: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Test Engineer",
+      company: "Western Digital",
+      companylogo: require("./assets/images/westernLogo.png"),
+      date: "Jun 2020 – Present",
+      descBullets: [
+        "Developing an internal test queue monitoring tool for scalability and reduction of test queue times.",
+        "Designing and creating a SQL database to store and record critical test time metrics.",
+        "Setup and validate new data gathering process to reduce continuous integration test plans.",
+      ],
+    },
+    {
+      role: "Information Systems Intern",
+      company: "Cornerstone OnDemand",
+      companylogo: require("./assets/images/cornerstoneLogo.jpg"),
+      date: "Jan 2020 – Apr 2020",
+      descBullets: [
+        "Testing and validating for accuracy of data to assist the development of an ETL process.",
+        "Developing reports and dashboards using Power BI to display critical business metrics.",
+      ],
+    },
+    {
+      role: "Software Developer Intern",
+      company: "IBM",
+      companylogo: require("./assets/images/ibmLogo.png"),
+      date: "Jun 2018 – Aug 2019",
+      descBullets: [
+        "Responsible for the design and implementation of an analytical dashboard for test analysis, display of story points, and interpretation of the product release health.",
+        "Involved in creating and enhancing test automations with Jenkins to increase efficiency of the testing process.",
+        "Participated in scrum meetings within the team and sprints to deliver progress.",
+      ],
+    },
+  ],
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  githubUserName: "dongxli", // Change to your github username to view your profile in Contact Section.
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to false
+};
+
+// Some Big Projects You have worked with your company
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/saayaHealthLogo.webp"),
+      link: "http://saayahealth.com/",
+    },
+    {
+      image: require("./assets/images/nextuLogo.webp"),
+      link: "http://nextu.se/",
+    },
+  ],
+};
+
+// Your Achievement Section Include Your Certification Talks and More
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "Google Code-In Finalist",
+      subtitle:
+        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+      image: require("./assets/images/codeInLogo.webp"),
+      footerLink: [
+        {
+          name: "Certification",
+          url:
+            "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing",
+        },
+        {
+          name: "Award Letter",
+          url:
+            "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing",
+        },
+        {
+          name: "Google Code-in Blog",
+          url:
+            "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html",
+        },
+      ],
+    },
+    {
+      title: "Google Assistant Action",
+      subtitle:
+        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image: require("./assets/images/googleAssistantLogo.webp"),
+      footerLink: [
+        {
+          name: "View Google Assistant Action",
+          url:
+            "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en",
+        },
+      ],
+    },
+
+    {
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      image: require("./assets/images/pwaLogo.webp"),
+      footerLink: [
+        { name: "Certification", url: "" },
+        {
+          name: "Final Project",
+          url: "https://pakistan-olx-1.firebaseapp.com/",
+        },
+      ],
+    },
+  ],
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+
+  blogs: [
+    {
+      url:
+        "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?",
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies.",
+    },
+  ],
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  talks: [
+    {
+      title: "Build Actions For Google Assistant",
+      subtitle: "Codelab at GDG DevFest Karachi 2019",
+      slides_url: "https://bit.ly/saadpasta-slides",
+      event_url: "https://www.facebook.com/events/2339906106275053/",
+    },
+  ],
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo",
+  ],
+};
+
+const contactInfo = {
+  title: emoji("Contact Me"),
+  subtitle: "",
+  // number: "+92-3243454077",
+  email_address: "dongxli710@gmail.com",
+};
+
+//Twitter Section
+
+const twitterDetails = {
+  userName: "twitter", //Replace "twitter" with your twitter username without @
+};
+export {
+  greeting,
+  socialMediaLinks,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+};
